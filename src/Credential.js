59888dd99cc3da3b37bd06c44c50acc89c3ed42c
dek-haji <deeeqhaji@gmail.com>
2020-03-16 11:49:51 -0500

    added user loggin

---
+++ src/Credential.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+class Credential extends React.Component{
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+}
