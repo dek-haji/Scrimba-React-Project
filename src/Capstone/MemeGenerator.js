028648480804bc6dbb9401b2d85cf3b394ec1411
dek-haji <deeeqhaji@gmail.com>
2020-03-17 15:44:02 -0500

    added new state of meme

--- src/Capstone/MemeGenerator.js
+++ src/Capstone/MemeGenerator.js
@@ -6,13 +6,16 @@ class MemeGenerator extends React.Component{
         this.state = {
             topText: "",
             bottomText: "",
-            randomImage: "http://i.imgflip.com/1bij.jpg"
+            randomImage: "http://i.imgflip.com/1bij.jpg",
+            AllMemeImgs: []
         }
     }
     componentDidMount() {
         fetch("https://api.imgflip.com/get_memes")
-            .then(allMemeImgs => {
-                return allMemeImgs.json();
+            .then(response => response.json())
+            .then(response => {
+                const { memes } = response.data
+                console.log(memes[0])
             })
     }
     render() {
